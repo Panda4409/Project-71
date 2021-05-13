6511abb392bc3dfee6e1bba26d82eda26756fd32
Nishka <68439163+Panda4409@users.noreply.github.com>
2021-05-13 19:51:19 -0400

    Add files via upload

---
+++ Project 70/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Stylesheet, View, Text, Image} from 'react-native';
+import {createAppCOntainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
